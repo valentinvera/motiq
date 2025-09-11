254abb79c75b877e82231c3cf1594c79d0b064c4
Valentín Vera <valentinvera2805@gmail.com>
2025-09-11 02:26:45 -0300

    feat: add wailist and email tRPC routers to the global tRPC router of app

--- src/integrations/trpc/routers/app.ts
+++ src/integrations/trpc/routers/app.ts
@@ -1,5 +1,10 @@
 import { createTRPCRouter } from "../init"
+import { waitlistRouter } from "./waitlist"
+import { emailRouter } from "./email"
 
-export const appRouter = createTRPCRouter({})
+export const appRouter = createTRPCRouter({
+  waitlist: waitlistRouter,
+  email: emailRouter,
+})
 
 export type AppRouter = typeof appRouter
