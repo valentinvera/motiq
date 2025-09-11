6b8848752ebacb1485dcbbca9b374178f218ee68
Valentín Vera <valentinvera2805@gmail.com>
2025-09-11 01:59:27 -0300

    feat: add import of the schema

--- src/db/drizzle.ts
+++ src/db/drizzle.ts
@@ -2,6 +2,7 @@ import { upstashCache } from "drizzle-orm/cache/upstash"
 import { drizzle } from "drizzle-orm/neon-http"
 import { neon } from "@neondatabase/serverless"
 import { serverOnly } from "@tanstack/react-start"
+import * as schema from "./schema"
 import { env } from "@/env/server"
 
 const sql = neon(env.DATABASE_URL)
@@ -10,6 +11,7 @@ const getDb = serverOnly(() =>
   drizzle({
     client: sql,
     casing: "snake_case",
+    schema,
     cache: upstashCache({
       url: env.UPSTASH_REDIS_REST_URL,
       token: env.UPSTASH_REDIS_REST_TOKEN,
